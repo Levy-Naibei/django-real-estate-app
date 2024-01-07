6502cb580c794b120be5b5ea80e89c685dd8ee49
Levy-Naibei <levynaibei2@gmail.com>
2024-01-07 23:39:38 +0300

    feat: added create user method

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -8,5 +8,32 @@ class CustomUserManager(BaseUserManager):
         try:
             validate_email(email)
         except ValidationError:
-            raise ValueError(_("Valid email address must be provided"))
+            raise ValueError(_("Valid email address must be provided."))
+    
+    def create_user(self, username, first_name, last_name, email, password, **extra_fields):
+        if not username:
+            raise ValueError(_("Username must be provided."))
+        if not first_name:
+            raise ValueError(_("First name must be provided."))
+        if not last_name:
+            raise ValueError(_("Last name must be provided."))
+        if email:
+            email = self.normalize_email(email)
+            self.email_validator(email)
+        else:
+            raise ValueError(_("Base User Account: An email is required."))
+        
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        user.save(using=self._db)
+        return user
     
