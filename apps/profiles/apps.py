0c10a4afb4247654966c344f7f0659e85d52f087
Levy-Naibei <levynaibei2@gmail.com>
2024-01-08 23:16:44 +0300

    feat: added create profile signal

--- apps/profiles/apps.py
+++ apps/profiles/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.profiles'
+
+    # make profile app to get notified of the signal
+    def ready(self):
+        from apps.profiles import signals
