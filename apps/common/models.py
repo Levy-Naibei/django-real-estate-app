e3d9a00e0049fd67ee38dfda5f97cfc0341ad5ba
Levy-Naibei <levynaibei2@gmail.com>
2024-01-08 17:58:09 +0300

    feat: added generic model to handle repeated fields

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class TimestampedUUIDModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+
+
+    # make the model abstract
+    class Meta:
+        abstract = True
