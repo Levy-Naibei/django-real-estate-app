b7911dc0afd9d1d6b1ab2cf3e142c52ca6adc326
Levy-Naibei <levynaibei2@gmail.com>
2024-01-11 14:55:13 +0300

    feat: added view for fetching agents

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Profile
+from .renderers import ProfileJSONRenderer
+from .exceptions import NotYourProfile, ProfileNotFound
+from .serializers import UpdateProfileSerializer, ProfileSerializer
+
+class AgentsListView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Profile.objects.filter(is_agent=True)
+    serializer_class = ProfileSerializer
