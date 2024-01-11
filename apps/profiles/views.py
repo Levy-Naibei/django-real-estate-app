10d2d8de172942ed2c17ac619d415dfbbf581d7a
Levy-Naibei <levynaibei2@gmail.com>
2024-01-11 14:58:40 +0300

    feat: added view for updating user profile

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -26,3 +26,30 @@ class GetProfileView(APIView):
         user_profile = Profile.objects.get(user=user)
         serializer = ProfileSerializer(user_profile, context={"request": request})
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+class UpdateProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [ProfileJSONRenderer]
+    serializer_class = UpdateProfileSerializer
+
+    def patch(self, request, username):
+        try:
+            Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            raise ProfileNotFound
+
+        user_name = request.user.username
+        if user_name != username:
+            raise NotYourProfile
+        
+        data = request.data
+        serializer = UpdateProfileSerializer(
+            data=data,
+            instance=request.user.profile,
+            partial=True
+        ) # partial because we are updating
+
+        serializer.is_valid()
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
