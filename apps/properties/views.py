84a021ea9bd9d4ddf8857debdcf3d167ba6d624b
Levy-Naibei <levynaibei2@gmail.com>
2024-01-16 14:46:09 +0300

    feat: added property filter

---
+++ apps/properties/views.py
@@ -0,0 +1,30 @@
+import logging
+
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics, permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .exceptions import PropertyNotFound
+from .pagination import PropertyPagination
+from .models import Property, PropertyViews
+from .serializers import (
+    PropertyCreateSerializer,
+    PropertyViewSerializer,
+    PropertySerializer
+)
+
+logger = logging.getLogger(__name__)
+
+class PropertyFilter(django_filters.FilterSet):
+    advert_type = django_filters.CharFilter(lookup_expr="iexact", field_name="advert_type")
+    property_type = django_filters.CharFilter(lookup_expr="iexact", field_name="property_type")
+    price = django_filters.NumberFilter()
+    price__gt = django_filters.NumberFilter(field_name="price", lookup_expr="gt")
+    price__lt = django_filters.NumberFilter(field_name="price", lookup_expr="lt")
+
+    class Meta:
+        model = Property
+        field = ["advert_type", "property_type", "price"]
