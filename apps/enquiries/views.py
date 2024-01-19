117e4b0509ca4e32e706f0564786137568ebde69
Levy-Naibei <levynaibei2@gmail.com>
2024-01-19 13:54:45 +0300

    feat: added enquiries view

---
+++ apps/enquiries/views.py
@@ -0,0 +1,35 @@
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.core.mail import send_mail
+
+from real_estate.settings.development import DEFAULT_FROM_EMAIL
+from .models import Enquiry
+
+
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def send_enquiry_email(request):
+    data = request.data
+    try:
+        name = data["name"]
+        subject = data["subject"]
+        email = data["email"]
+        from_email = data["email"]
+        phone_number = data["phone_number"]
+        message = data["message"]
+        recipient_list = [DEFAULT_FROM_EMAIL]
+
+        send_mail(from_email, subject, message, recipient_list, fail_silently=True)
+        enquiry = Enquiry(
+            name=name,
+            email=email,
+            phone_number=phone_number,
+            message=message,
+            subject=subject,
+        )
+        enquiry.save()
+        return Response({"Success": "Enquiry email submitted successfully"})
+
+    except:
+        return Response({"Error": "Error occured while trying to send"})
