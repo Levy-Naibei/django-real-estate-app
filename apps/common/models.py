cb3da1b40685a8b8f1d5c4c33108574aba13378d
Levy-Naibei <levynaibei2@gmail.com>
2024-01-19 12:48:10 +0300

    chore: renamed field

--- apps/common/models.py
+++ apps/common/models.py
@@ -5,7 +5,7 @@ class TimestampedUUIDModel(models.Model):
     pkid = models.BigAutoField(primary_key=True, editable=False)
     id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 
     # make the model abstract
