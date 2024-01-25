1aab46458f087de181f426d4ab545e8ec0271d29
Levy-Naibei <levynaibei2@gmail.com>
2024-01-25 23:37:57 +0300

    ref: django model factory import

--- tests/factories.py
+++ tests/factories.py
@@ -2,12 +2,13 @@ import factory
 from apps.profiles.models import Profile
 from django.db.models.signals import post_save
 from faker import Faker as FakerFactory
+from factory.django import DjangoModelFactory
 from real_estate.settings.base import AUTH_USER_MODEL
 
 faker = FakerFactory.create()
 
 @factory.django.mute_signals(post_save)
-class ProfileFactory(factory.django.DjangoModelFactory):
+class ProfileFactory(DjangoModelFactory):
     user = factory.SubFactory("tests.factories.UserFactory")
     phone_number = factory.LazyAttribute(lambda x: faker.phone_number())
     about_me = factory.LazyAttribute(lambda x: faker.sentence(nb_words=5))
@@ -27,7 +28,7 @@ class ProfileFactory(factory.django.DjangoModelFactory):
         model = Profile
 
 @factory.django.mute_signals(post_save)
-class UserFactory(factory.django.DjangoModelFactory):
+class UserFactory(DjangoModelFactory):
     first_name = factory.LazyAttribute(lambda x: faker.first_name())
     last_name = factory.LazyAttribute(lambda x: faker.last_name())
     username = factory.LazyAttribute(lambda x: faker.first_name())
