22f2721560ffe9babf78e8436a899d1cff01e794
Levy-Naibei <levynaibei2@gmail.com>
2024-01-30 23:46:31 +0300

    chore: added celery env vars and watchdog libs

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -28,9 +28,9 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 # Application definition
 
 DJANGO_APPS = [
+    "django.contrib.contenttypes",
     "django.contrib.admin",
     "django.contrib.auth",
-    "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Africa/Nairobi"
 
 USE_I18N = True
 
