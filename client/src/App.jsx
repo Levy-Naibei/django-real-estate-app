c4996e2f69f60b633eb903353e8e0d75a70374ce
Levy-Naibei <levynaibei2@gmail.com>
2024-02-07 14:41:54 +0300

    feat: added toastcontainer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 import Property from "./pages/Property";
@@ -10,9 +11,12 @@ const App = () => {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/properties" element={<Property />} />
+          {/* <main className="py-3"> */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/properties" element={<Property />} />
+          {/* </main> */}
         </Routes>
+        <ToastContainer theme="dark" />
         <Footer />
       </Router>
     </>
