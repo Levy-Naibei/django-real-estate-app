781f947eefad53367ae403b16a695a7ee6eccb06
Levy-Naibei <levynaibei2@gmail.com>
2024-02-09 15:38:38 +0300

    chore: added loader spinner and helmet deps

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+// import ScaleLoader from "react-spinners/ScaleLoader";
 
 const Spinner = () => {
   return (
     <div className="spinner-container">
       <div className="spinner"></div>
+      {/* <ScaleLoader color={"#04AA6D"}/>; */}
     </div>
-  )
-}
+  );
+};
 
-export default Spinner
+export default Spinner;
