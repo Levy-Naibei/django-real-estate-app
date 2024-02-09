8c9521131322377fa3061c71c78da922dc849aa2
Levy-Naibei <levynaibei2@gmail.com>
2024-02-09 15:48:19 +0300

    chore: reused Title component to updated doc title

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import { Container, Button } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import Title from "../components/Title";
 
 const LandingPage = () => {
   return (
     <header className="masthead main-bg-image">
       <Container className="h-100 d-flex align-items-center justify-content-center px-4 px-lg-5">
+        <Title />
         <div className="justify-content-center">
           <div className="text-center">
             <h1 className="text-uppercase mx-auto my-0">Real Estate</h1>
